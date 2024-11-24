0a4eedfcc85ee9cfdb55d966589503873bd0eb8b
dutenrapha <dutenrapha@hotmail.com>
2024-11-24 19:54:40 -0300

    fix 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-descriptor=$(bitcoin-cli getdescriptorinfo "tr(${xpub}/0/100)" | jq -r '.descriptor')
-taproot_address=$(bitcoin-cli deriveaddresses "$descriptor" | jq -r '.[0]')
-echo $taproot_address
 
+# Configuração inicial
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+
+# Criar um descritor para o caminho derivado
+descriptor=$(bitcoin-cli getdescriptorinfo "tr(${xpub}/100)" | jq -r '.descriptor')
+
+echo $descriptor
+# Derivar o endereço Taproot
+taproot_address=$(bitcoin-cli deriveaddresses "$descriptor" | jq -r '.[0]')
+
+
+# Exibir o endereço derivado
+echo $taproot_address
