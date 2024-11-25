0fab341a50e7fdbe73f01ac7895b6ec51dae0ea7
dutenrapha <dutenrapha@hotmail.com>
2024-11-24 23:51:00 -0300

    fix 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,4 +1,20 @@
-hash=$(bitcoin-cli getblockhash 123321)
-block=$(bitcoin-cli getblock $hash 2)
-unspent=$(echo $block | jq '.tx[].vout[] | select(.scriptPubKey.type == "pubkeyhash") | .scriptPubKey.addresses[0]')
-echo $unspent
+#!/bin/bash
+BLOCK_HASH=$(bitcoin-cli getblockhash 123321)
+
+TXS=$(bitcoin-cli getblock "$BLOCK_HASH" | jq -r '.tx[]' 2>/dev/null)
+LAST_UTXO_ADDRESS=""
+for TX in $TXS; do
+  RAW_TX=$(bitcoin-cli getrawtransaction "$TX" true 2>/dev/null)
+  VOUTS=$(echo "$RAW_TX" | jq -c '.vout[]' 2>/dev/null)
+  while IFS= read -r VOUT; do
+    INDEX=$(echo "$VOUT" | jq -r '.n // empty')
+    ADDRESS=$(echo "$VOUT" | jq -r '.scriptPubKey.address // empty')
+    TXOUT=$(bitcoin-cli gettxout "$TX" "$INDEX" 2>/dev/null)
+
+    if [ -n "$TXOUT" ] && [ "$TXOUT" != "null" ]; then
+      LAST_UTXO_ADDRESS="$ADDRESS"
+    fi
+  done <<< "$VOUTS"
+done
+
+echo $LAST_UTXO_ADDRESS
